5fc9499e85a274378ba27f6096891be2f52eed99
donatin48 <40335400+donatin48@users.noreply.github.com>
2021-12-19 19:54:18 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from pronotepy.ent import ile_de_france as ent # select your ent 
+user = "" # your username
+password = ""   # your password
+# your discord webhook url
+url_webhook = ""
+# your pronote redirection url
+url_redirection = ""
